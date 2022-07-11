55c1c50eb91bbd5801ddd2e663a6f56e3bfdb3e3
Bekbolat Kuralbayev <bkb@live.ru>
2022-07-11 18:42:45 +0600

    add solution - try 2

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -13,18 +13,25 @@ func getOne(id int64) user {
 	return user{ID: id}
 }
 
-// getBatch takes two arguments - the number of users and the number of goroutines in which users will concurrently load.
-// It returns an array of received users.
+// getBatch concurrently loads n users from the database.
+// The number of goroutines used to load the users is determined by the pool parameter.
+// The function returns a slice of users.
+// The function should return a slice of users with the same length as the n parameter.
 func getBatch(n int64, pool int64) (res []user) {
 
+	// channel to receive the users
 	ch := make(chan user)
 
+	// create a goroutine for each user
 	for i := int64(0); i < pool; i++ {
-		go func(id int64) {
-			ch <- getOne(id)
+		go func(i int64) {
+			for j := i; j < n; j += pool {
+				ch <- getOne(j)
+			}
 		}(i)
 	}
 
+	// collect the users
 	for i := int64(0); i < n; i++ {
 		res = append(res, <-ch)
 	}
