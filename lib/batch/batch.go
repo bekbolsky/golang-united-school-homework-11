99e741f49337f47a292f340d95d6de0c1a6fe143
Bekbolat Kuralbayev <bkb@live.ru>
2022-07-14 12:30:38 +0600

    solution with waitgroup approach

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -18,23 +19,38 @@ func getOne(id int64) user {
 // The function returns a slice of users.
 // The function should return a slice of users with the same length as the n parameter.
 func getBatch(n int64, pool int64) (res []user) {
+	// create a slice of users with n capacity
+	res = make([]user, 0, n)
 
-	// channel to receive the users
-	ch := make(chan user)
+	// create variable to hold the wait group
+	// and add the number of goroutines to it
+	// so that we know when all goroutines have finished
+	var wg sync.WaitGroup
 
-	// create a goroutine for each user
-	for i := int64(0); i < pool; i++ {
-		go func(i int64) {
-			for j := i; j < n; j += pool {
-				ch <- getOne(j)
+	// add a number of goroutines to the wait group
+	// the number of goroutines is determined by the pool parameter
+	wg.Add(int(pool))
+
+	// iterate over the number of goroutines
+	for i := 0; i < int(pool); i++ {
+		// create a gorroutine that loads batch of users
+		go func(i int) {
+			// iterate over the batch of users
+			// the number of users is determined by the n parameter
+			// the number of users loaded by each goroutine is determined by the pool parameter
+			for j := int64(i); j < n; j += int64(pool) {
+				// append the result of getOne() to the slice
+				// this will block until the goroutine is finished
+				res = append(res, getOne(j))
 			}
+			// when the goroutine is done, decrement the wait group
+			wg.Done()
 		}(i)
 	}
-
-	// collect the users
-	for i := int64(0); i < n; i++ {
-		res = append(res, <-ch)
-	}
+	// wait for all goroutines to finish
+	// this blocks the main goroutine
+	// until all goroutines have finished
+	wg.Wait()
 
 	return res
 }
